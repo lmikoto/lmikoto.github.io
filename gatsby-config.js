70b8412508128a65b403441aa7f5369996c3943e
lmikoto <lmikoto@outlook.com>
2020-01-04 18:08:07 +0800

    feat: scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
